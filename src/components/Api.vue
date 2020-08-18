29517ae6f01a247973de478952427ea39dceff7a
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-08-18 15:50:00 +0900

    Create Api.vue

---
+++ src/components/Api.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="api">
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Api',
+  components: {
+    api
+  }
+}
+</script>
+
+<style>
+#api {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
